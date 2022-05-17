d14842e0a94162ba7f38a3eb0f55bd19f7e8866f
LIMPIX31 <limpix31@gmail.com>
2022-05-17 17:26:25 +0300

    TimeUpdatePacket

--- src/packets/states/play/TimeUpdatePacket.ts
+++ src/packets/states/play/TimeUpdatePacket.ts
@@ -1,5 +1,23 @@
-import { Packet } from '../../Packet'
+import { Packet, PacketState } from '../../Packet'
+import { long } from '../../../alias'
 
 export class TimeUpdatePacket extends Packet {
+  static state: number = PacketState.Play
+  static type: number = 0x59
 
+  worldAge: long = 0n
+  time: long = 6000n
+
+  constructor() {
+    super(TimeUpdatePacket.state, TimeUpdatePacket.type)
+  }
+
+  encode() {
+    this.writeLong(this.worldAge, this.time)
+  }
+
+  decode() {
+    this.worldAge = this.readLong()
+    this.time = this.readLong()
+  }
 }
